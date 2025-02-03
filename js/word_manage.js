dac0cd805252afea1a55840ce019aac4cce4b225
charles <charlesyin2020@mail.com>
2025-02-03 22:46:45 +0900

    fix

--- js/word_manage.js
+++ js/word_manage.js
@@ -88,7 +88,7 @@ async function login() {
 }
 
 
-async function showWordManageForm(ses) {
+async function showWordManageForm() {
     const container = document.getElementById('manage-setup');
 
     container.innerHTML = `
@@ -107,6 +107,13 @@ async function showWordManageForm(ses) {
 
     const sourceDatas = await getSourceList();
 
+    console.log(sourceDatas);
+    // if (sourceDatas.id === 403) {
+    //     console.error('获取资源列表失败,重新登陆');
+    //     alert('获取资源列表失败,重新登陆');
+    //     showLoginForm();
+    // }
+
     let sourceContent = '<option value="">请选择单词资源</option>';
     for (let item of sourceDatas) {
         sourceContent = sourceContent + `\n<option value="${item.id}">${item.name}</option>`;
@@ -117,45 +124,96 @@ async function showWordManageForm(ses) {
 
     const searchButton = document.getElementById('search-but');
 
+    const wordInputor = document.getElementById('word-input');
+
     searchButton.addEventListener('click', async function() {
         const data =  await wordSearch();
-        await showWordDetail(data);
+        const wordInput = wordInputor.value;
+        await showWordDetail(data, wordInput);
     });
 }
 
 
-async function showWordDetail(datas) {
+async function showWordDetail(datas, word) {
     const container = document.getElementById('manage-setup');
 
     const sourceList = await getSourceList();
 
     container.innerHTML = '';
 
-    for (let data of datas) {
+    if (datas.length > 0) {
+        for (let data of datas) {
+            container.innerHTML +=`
+                <div class="word-detail-form">
+                    <input id="word-id-${data.id}" value=${data.id} style="display: none" />
+                    <div class="form-group">
+                        <label class="word-detail-lab">单词:</label>
+                        <input id="word-in-${data.id}" value="${data.word || ''}" />
+                    </div> 
+                    <div class="form-group">
+                        <label class="word-detail-lab">读音:</label>
+                        <input id="reading-in-${data.id}" value="${data.pronunciation || '' }" />
+                    </div>
+                    <div class="form-group">
+                        <label class="word-detail-lab">中文意思:</label>
+                        <input id="zh-meaning-in-${data.id}" value="${data.zh_meaning || ''}" />
+                    </div>
+                    <div class="form-group">
+                        <label class="word-detail-lab">英文意思:</label>
+                        <input id="en-meaning-in-${data.id}" value="${data.en_meaning || ''}" />
+                    </div>
+                    <div class="form-group">
+                        <label class="word-detail-lab">单词来源:</label>
+                        <select id="source-in-${data.id}">
+                            <option value="">请选择单词来源</option>
+                            ${sourceList.map(source => `
+                                <option value="${source.id}" ${source.id === data.resource ? 'selected' : ''}>
+                                    ${source.name}
+                                </option>
+                            `).join('')}
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label class="word-detail-lab">课时:</label>
+                        <input id="lesson-in-${data.id}" value=${data.lesson || ''} />
+                    </div>
+                    <div class="form-group">
+                        <label class="word-detail-lab">等级:</label>
+                        <input id="level-in-${data.id}" value=${data.level || ''} />
+                    </div>
+                    <div id="is-main-container">
+                        <label class="word-detail-lab">是否是附加课程</label>
+                        <input type="checkbox" id="is-main-${data.id}" ${data.is_main === 0 ? 'checked' : ''} />
+                    </div>
+                </div>
+            `;
+        }
+    } else {
         container.innerHTML +=`
             <div class="word-detail-form">
+                <input id="word-id-0" value=0 style="display: none" />
                 <div class="form-group">
                     <label class="word-detail-lab">单词:</label>
-                    <input id="word-in-${data.id}" value="${data.word || ''}" />
+                    <input id="word-in-0" value="${word}" />
                 </div> 
                 <div class="form-group">
                     <label class="word-detail-lab">读音:</label>
-                    <input id="reading-in-${data.id}" value="${data.pronunciation || '' }" />
+                    <input id="reading-in-0" value="" />
                 </div>
                 <div class="form-group">
                     <label class="word-detail-lab">中文意思:</label>
-                    <input id="zh-meaning-in-${data.id}" value="${data.zh_meaning || ''}" />
+                    <input id="zh-meaning-in-0" value="" />
                 </div>
                 <div class="form-group">
                     <label class="word-detail-lab">英文意思:</label>
-                    <input id="en-meaning-in-${data.id}" value="${data.en_meaning || ''}" />
+                    <input id="en-meaning-in-0" value="" />
                 </div>
                 <div class="form-group">
                     <label class="word-detail-lab">单词来源:</label>
-                    <select id="source-in-${data.id}">
+                    <select id="source-in-0">
                         <option value="">请选择单词来源</option>
                         ${sourceList.map(source => `
-                            <option value="${source.id}" ${source.id === data.resource ? 'selected' : ''}>
+                            <option value="${source.id}"}>
                                 ${source.name}
                             </option>
                         `).join('')}
@@ -163,15 +221,15 @@ async function showWordDetail(datas) {
                 </div>
                 <div class="form-group">
                     <label class="word-detail-lab">课时:</label>
-                    <input id="lesson-in-${data.id}" value=${data.lesson || ''} />
+                    <input id="lesson-in-0" value="" />
                 </div>
                 <div class="form-group">
                     <label class="word-detail-lab">等级:</label>
-                    <input id="level-in-${data.id}" value=${data.level || ''} />
+                    <input id="level-in-0" value="" />
                 </div>
                 <div id="is-main-container">
                     <label class="word-detail-lab">是否是附加课程</label>
-                    <input type="checkbox" id="is-main-${data.id}" ${data.is_main === 0 ? 'checked' : ''} />
+                    <input type="checkbox" id="is-main-0" value="" />
                 </div>
             </div>
         `;
@@ -184,32 +242,91 @@ async function showWordDetail(datas) {
         </div>
     `;
 
-    let lessonContent = '<option value="">请选择课时</option>';
-    let levelContent = '<option value="">请选择级别</option>';
-
-
-
-    // const saveButton = document.getElementById("save-word");
+    const saveButton = document.getElementById("save-word");
     // const returnButton = document.getElementById("return-search-page");
     
-    // saveButton.addEventListener("click", () =>{
-    //         if (data.id) {
-    //             wordUpdate();
-    //         } else {
-    //             wordInsert();
-    //         }
-    //     }
-    // );
+    saveButton.addEventListener("click", async () =>{
+            if (datas.length === 0) {
+                const newWord = new Object({id: 0});
+                
+                const wordIn = document.getElementById('word-in-0');
+                const readinIn = document.getElementById('reading-in-0');
+                const zhMeaningIn = document.getElementById('zh-meaning-in-0');
+                const enMeaningIn = document.getElementById('en-meaning-in-0');
+                const sourceIn = document.getElementById('source-in-0');
+                const lessonIn = document.getElementById('lesson-in-0');
+                const levelIn = document.getElementById('level-in-0');
+                const isMainIn = document.getElementById('is-main-0');
+
+                Object.assign(newWord, {
+                    word: wordIn.value,
+                    pronunciation: readinIn.value? readinIn.value : null,
+                    zh_meaning: zhMeaningIn.value,
+                    en_meaning: enMeaningIn.value? enMeaningIn.value : null,
+                    resource: Number(sourceIn.value),
+                    lesson: Number(lessonIn.value),
+                    level: Number(levelIn.value),
+                    is_main: isMainIn.checked? 0 : 1
+                });
+
+                console.log(newWord);
+                const result = await wordInsert(newWord);
+                console.log(`${newWord.word} insert result:`, result);
+                alter(`${newWord.word} insert result:`, result);
+            } else {
+                for (let item of datas) {
+                    const correctedWord = new Object({id: item.id});
+                    const wordIn = document.getElementById(`word-in-${item.id}`);
+                    const readinIn = document.getElementById(`reading-in-${item.id}`);
+                    const zhMeaningIn = document.getElementById(`zh-meaning-in-${item.id}`);
+                    const enMeaningIn = document.getElementById(`en-meaning-in-${item.id}`);
+                    const sourceIn = document.getElementById(`source-in-${item.id}`);
+                    const lessonIn = document.getElementById(`lesson-in-${item.id}`);
+                    const levelIn = document.getElementById(`level-in-${item.id}`);
+                    const isMainIn = document.getElementById(`is-main-${item.id}`);
+
+                    Object.assign(correctedWord, {
+                        word: wordIn.value,
+                        pronunciation: readinIn.value? readinIn.value : null,
+                        zh_meaning: zhMeaningIn.value,
+                        en_meaning: enMeaningIn.value? enMeaningIn.value : null,
+                        resource: Number(sourceIn.value),
+                        lesson: Number(lessonIn.value),
+                        level: Number(levelIn.value),
+                        is_main: isMainIn.checked? 0 : 1
+                    });
+
+                    if (!objectShallowEqual(item, correctedWord)) {
+                        const result = await wordUpdate(correctedWord);
+
+                        console.log(`${item.word} update status:`, result);
+                        alert(`${item.word} update status: ${result}`);
+                    } else {
+                        console.log(`${item.word} no need to update`);
+                        alert(`${item.word} no need to update`);
+                    }
+                }
+            }
+        }
+    );
 
     // returnButton.addEventListener("click", () => {
     //     showWordManageForm();
     // });
 }
 
+function objectShallowEqual(obj1, obj2) {
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+
+    if (keys1.length !== keys2.length) return false;
+
+    return keys1.every((key) => obj1[key] === obj2[key]);
+}
+
 
 async function wordSearch() {
     const ses = getCookie('ses');
-    console.log('ses:', ses);
     const searchUrl = `https://jl.charlesyin20218621.workers.dev/words/search?ses=${ses}`;
 
     try {
@@ -244,56 +361,63 @@ async function wordSearch() {
 }
 
 
-async function wordUpdate() {
-    const updateUrl = '';
+async function wordUpdate(word) {
+    const ses = getCookie('ses');
+    const updateUrl = `https://jl.charlesyin20218621.workers.dev/words/correct?ses=${ses}`;
 
     try {
-        return new Promise((resolve, reject) => {
-            const response = fetch(
-                updateUrl, {
-                    method: 'post'
-                }
-            );
-    
-            const updateResult = response.json();
-    
-            if (updateResult.error) {
-                alert(updateResult.error);
-                reject(updateResult.error);
+        const response = await fetch(
+            updateUrl, {
+                method: 'post',
+                body: JSON.stringify({
+                    'word': word
+                })
             }
+        );
 
-            resolve(updateResult.data);
-        });
+        const updateResult = await response.json();
+
+        if (updateResult.error) {
+            console.error('更新单词失败:', updateResult.error);
+            alert(updateResult.error);
+        }
+
+        return updateResult.status;
     } catch (error) {
         console.error('更新单词失败:', error);
         alert('更新单词失败，请重试');
+        return;
     }
 }
 
 
-async function wordInsert() {
-    const insertUrl = '';
+async function wordInsert(word) {
+    const ses = getCookie('ses');
+    const insertUrl = `https://jl.charlesyin20218621.workers.dev/words/create?ses=${ses}`;
 
     try {
-        return new Promise((resolve, reject) => {
-            const response = fetch(
-                insertUrl, {
-                    method: 'post'
-                }
-            );
-
-            const insertResult = response.json();
-
-            if (insertResult.error) {
-                alert(insertResult.error);
-                reject(insertResult.error);
+        const response = await fetch(
+            insertUrl, {
+                method: 'post',
+                body: JSON.stringify({
+                    word: word
+                })
             }
+        );
 
-            resolve(insertResult.data);
-        });
+        const insertResult = await response.json();
+
+        if (insertResult.error) {
+            console.log('创建单词失败:', insertResult.error);
+            alert(insertResult.error);
+            return;
+        }
+
+        return insertResult.msg;
     } catch (error) {
         console.error('新增单词失败:', error);
         alert('新增单词失败，请重试');
+        return;
     }
 }
 
@@ -315,11 +439,10 @@ async function getSourceList() {
         );
 
         const sourceData = await response.json();
-
         if (sourceData.error) {
-            alert(data.error);
+            alert(sourceData.error);
             
-            if (data.error === 'ses is incorrect') {
+            if (sourceData.id === 403) {
                 showLoginForm();
             }
             return;
@@ -338,7 +461,7 @@ function setCookie(name, value, daysToExpire = 0.5, path = '/') {
     const date = new Date();
     date.setTime(date.getTime + (daysToExpire * 24 * 60 * 60 * 1000));
     const expires = `expires=${date.toUTCString()}`;
-    document.cookie = `${name}=${encodeURIComponent(value)}; ${expires}; path=${path}`;
+    document.cookie = `${name}=${encodeURIComponent(value)}; ${expires}; path=${path}; SameSite=Lax`;
 }
 
 function getCookie(name) {
